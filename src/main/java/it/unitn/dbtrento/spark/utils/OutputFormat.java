d425a1caaf4e3118e9a06bd6f249476c99977f23
Daniele Foroni <daniele.foroni@gmail.com>
2018-01-19 00:59:50 +0100

    released version 1.0-SNAPSHOT

--- src/main/java/it/unitn/dbtrento/spark/utils/OutputFormat.java
+++
@@ -1,5 +0,0 @@
-package it.unitn.dbtrento.spark.utils;
-
-public enum OutputFormat {
-  CSV, PARQUET, SINGLE_CSV
-}
--- src/main/scala/eu/unitn/disi/db/spark/io/FSType.scala
+++
@@ -1,5 +0,0 @@
-package eu.unitn.disi.db.spark.io
-
-object FSType extends Enumeration {
-  val FS, HDFS = Value
-}
